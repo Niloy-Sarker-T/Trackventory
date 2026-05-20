b7cedaae5e012e9b16790a8a36bfd4cbe92fd1c5
Niloy-Sarker-T <niloysarker0033@gmail.com>
2026-05-20 07:23:38 +0600

    API

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
           <span className="ml-3 text-xl">NST Shop</span>
         </span>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2026 MR Tech <span className="text-gray-600 ml-1">@Mashod Rana</span>
+          ©  <span className="text-gray-600 ml-1"></span>
         </p>
       </div>
     </footer>
